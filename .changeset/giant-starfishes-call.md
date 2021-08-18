0ad794ab5edfa75cc3e0ef073f765b6ab9de0fb8
Jake Ginnivan <jake@ginnivan.net>
2021-08-18 22:48:58 +0800

    Fixed useFeature hook not returning current value during inital render

---
+++ .changeset/giant-starfishes-call.md
@@ -0,0 +1,5 @@
+---
+'@featureboard/react-sdk': patch
+---
+
+Fixed useFeature hook not returning current value during inital render
--- libs/react-sdk/src/lib/use-feature.ts
+++ libs/react-sdk/src/lib/use-feature.ts
@@ -7,15 +7,20 @@ export function useFeature<T extends keyof Features>(
     defaultValue: Features[T],
 ): Features[T] {
     const fbContext = React.useContext(FeatureBoardContext)
-    const [value, setValue] = React.useState(defaultValue)
+    const [value, setValue] = React.useState(
+        fbContext?.client.getFeatureValue(featureKey, defaultValue) ||
+            defaultValue,
+    )
 
     if (!fbContext?.client) {
         throw new Error('Ensure FeatureBoardProvider is set')
     }
 
     React.useEffect(() => {
-        return fbContext.client.subscribeToFeatureValue(featureKey, defaultValue, (value) =>
-            setValue(value),
+        return fbContext.client.subscribeToFeatureValue(
+            featureKey,
+            defaultValue,
+            (value) => setValue(value),
         )
     })
 
