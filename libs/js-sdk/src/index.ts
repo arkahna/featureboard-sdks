2fd34fb1c9b70904eaf63d863743b1c893d6f611
Jake Ginnivan <jake@ginnivan.net>
2021-08-21 17:35:13 +0800

    Fixed paths

--- libs/js-sdk/src/index.ts
+++ libs/js-sdk/src/index.ts
@@ -1,8 +1,8 @@
-export * from './lib/client'
-export * from './lib/effective-feature-state'
-export * from './lib/featureboard-api-config'
-export * from './lib/featureboard-service-urls'
-export * from './lib/features-client'
-export * from './lib/live-connection'
+export * from './client'
+export * from './effective-feature-state'
+export * from './featureboard-api-config'
+export * from './featureboard-service-urls'
+export * from './features-client'
+export * from './live-connection'
 
 export interface Features {}
