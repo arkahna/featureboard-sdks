4fa657327e912464509de444a49bdf06b23894ed
Jake Ginnivan <jake@ginnivan.net>
2022-01-02 13:11:41 +0800

    Cleanup jest/babel

--- libs/live-connection/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  displayName: 'live-connection',
-  preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/live-connection',
-}
--- libs/react-sdk/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  displayName: 'react-sdk',
-  preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/react-sdk',
-}
