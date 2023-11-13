ae8891e7538269bbf57230af74aaa5df629f390f
Matthew Nixon <matthew.nixon@arkahna.io>
2023-11-13 16:19:09 +0800

    added enum support

--- libs/java-sdk/src/main/java/featureboard/java/sdk/FeatureBoardClientImpl.java
+++ libs/java-sdk/src/main/java/featureboard/java/sdk/FeatureBoardClientImpl.java
@@ -61,9 +61,11 @@ public class FeatureBoardClientImpl implements FeatureBoardClient {
   public <TProp extends Enum<TProp>> TProp getFeatureValue(String featureKey, TProp defaultValue) {
     var jsonValue = getFeatureConfigurationValue(featureKey);
 
-    // TODO: fixme
-//    return jsonTryGetValueEnum(jsonValue, defaultValue)
-    return null;
+    if (jsonValue != null) {
+      return Enum.valueOf(defaultValue.getDeclaringClass(), jsonValue.asText());
+    }
+
+    return defaultValue;
   }
 
   @Override
@@ -72,9 +74,6 @@ public class FeatureBoardClientImpl implements FeatureBoardClient {
   }
 
   private JsonNode getFeatureConfigurationValue(String featureKey) {
-    // Current issue: jumps right in to fetching state which will be empty
-    // Need to ensure state is populated from the get go?
-
     // check state here, fetch snapshot
     // Just being defensive
     if (_snapshotState == null) {
@@ -101,15 +100,5 @@ public class FeatureBoardClientImpl implements FeatureBoardClient {
 
     _logger.fine(String.format("GetFeatureConfigurationValue: {audienceExceptionValue: %s, defaultValue: %s, value: %s}", (audienceException != null) ? audienceException.value() : "null", feature.defaultValue(), value));
     return value;
-  }
-
-  // TODO: do I need this?
-  private static <T extends Enum<T>> Optional<T> jsonTryGetValueEnum(JsonValue jsonValue, Class<T> enumType) {
-    try {
-      T enumValue = Enum.valueOf(enumType, jsonValue.toString().toUpperCase());
-      return Optional.of(enumValue);
-    } catch (IllegalArgumentException e) {
-      return Optional.empty();
-    }
   }
 }
