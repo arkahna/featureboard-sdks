24b9f01337a1c02f333c08e1c4fb9be52500a996
Ida Danielsson <ida.danielsson@arkahna.io>
2024-01-17 13:15:11 +0800

    reset error in ensure single

--- libs/js-sdk/src/ensure-single.ts
+++ libs/js-sdk/src/ensure-single.ts
@@ -14,6 +14,7 @@ export function createEnsureSingleWithBackoff<T>(
         ) {
             return Promise.reject(tooManyRequestsError)
         }
+        tooManyRequestsError = undefined
         if (!current) {
             current = cb()
                 .catch((error: Error) => {
